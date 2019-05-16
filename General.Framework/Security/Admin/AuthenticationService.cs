b407b1f75a7bdba96e70f90c4e905335701f96d2
谢冰 <xiebbing@126.com>
2019-05-16 22:51:18 +0800

    add user

--- General.Framework/Security/Admin/AuthenticationService.cs
+++ General.Framework/Security/Admin/AuthenticationService.cs
@@ -5,6 +5,10 @@ namespace General.Framework.Security.Admin
 {
     public class AuthenticationService:IAuthenticationService
     {
+        /// <summary>
+        /// 获取当前用户信息
+        /// </summary>
+        /// <returns></returns>
         public SysUser GetCurrentUser()
         {
             return new SysUser()
